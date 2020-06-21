9a00370323b8e1461ac9c11691f80ceb6479f282
rekols <rekols@foxmail.com>
2020-06-21 16:57:02 +0800

    Show window state operation buttons when the window is maximized

--- topbar/appmenuwidget.cpp
+++ topbar/appmenuwidget.cpp
@@ -3,22 +3,46 @@
 #include <QHBoxLayout>
 #include <QDebug>
 #include <QMenu>
+#include <QX11Info>
 
 #include <QDBusConnection>
 #include <QDBusConnectionInterface>
 #include <QDBusServiceWatcher>
 
-#include <KWindowSystem>
+#include <KF5/KWindowSystem/KWindowSystem>
+#include <KF5/KWindowSystem/KWindowInfo>
+#include <KF5/KWindowSystem/NETWM>
 
 AppMenuWidget::AppMenuWidget(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent),
+      m_minButton(new QToolButton),
+      m_restoreButton(new QToolButton),
+      m_closeButton(new QToolButton)
 {
     QHBoxLayout *layout = new QHBoxLayout;
     setLayout(layout);
 
+    m_buttonsWidget = new QWidget;
+    QHBoxLayout *buttonsLayout = new QHBoxLayout(m_buttonsWidget);
+    buttonsLayout->setContentsMargins(0, 0, 0, 0);
+    buttonsLayout->setSpacing(3);
+    buttonsLayout->addWidget(m_minButton);
+    buttonsLayout->addWidget(m_restoreButton);
+    buttonsLayout->addWidget(m_closeButton);
+    m_buttonsWidget->setVisible(false);
+
+    QSize iconSize(32, 32);
+    m_minButton->setIcon(QIcon(":/resources/min.svg"));
+    m_restoreButton->setIcon(QIcon(":/resources/restore.svg"));
+    m_closeButton->setIcon(QIcon(":/resources/close.svg"));
+    m_minButton->setIconSize(iconSize);
+    m_restoreButton->setIconSize(iconSize);
+    m_closeButton->setIconSize(iconSize);
+
     m_menuBar = new QMenuBar(this);
     m_menuBar->setAttribute(Qt::WA_TranslucentBackground);
     m_menuBar->setStyleSheet("background: transparent");
+    layout->addWidget(m_buttonsWidget);
     layout->addWidget(m_menuBar, 0, Qt::AlignVCenter);
     layout->setContentsMargins(0, 0, 0, 0);
 
@@ -26,8 +50,20 @@ AppMenuWidget::AppMenuWidget(QWidget *parent)
     menuImporter->connectToBus();
 
     m_appMenuModel = new AppMenuModel(this);
-
     connect(m_appMenuModel, &AppMenuModel::modelNeedsUpdate, this, &AppMenuWidget::updateMenu);
+    connect(KWindowSystem::self(), &KWindowSystem::activeWindowChanged, this, &AppMenuWidget::onActiveWindowChanged);
+    connect(KWindowSystem::self(), static_cast<void (KWindowSystem::*)(WId, NET::Properties, NET::Properties2)>(&KWindowSystem::windowChanged), this,
+            [=] (WId id, NET::Properties properties, NET::Properties2 properties2) {
+        if (properties.testFlag(NET::WMGeometry)) {
+            onActiveWindowChanged(id);
+        }
+    });
+
+    connect(m_minButton, &QToolButton::clicked, this, &AppMenuWidget::minimizeWindow);
+    connect(m_closeButton, &QToolButton::clicked, this, &AppMenuWidget::clsoeWindow);
+    connect(m_restoreButton, &QToolButton::clicked, this, &AppMenuWidget::restoreWindow);
+
+    onActiveWindowChanged(KWindowSystem::activeWindow());
 }
 
 void AppMenuWidget::updateMenu()
@@ -45,4 +81,44 @@ void AppMenuWidget::updateMenu()
         }
         m_menuBar->setVisible(true);
     }
+}
+
+void AppMenuWidget::toggleMaximizeWindow()
+{
+    KWindowInfo info(KWindowSystem::activeWindow(), NET::WMState);
+    bool isMax = info.hasState(NET::Max);
+
+    if (isMax) {
+        restoreWindow();
+    } else {
+        maxmizeWindow();
+    }
+}
+
+void AppMenuWidget::onActiveWindowChanged(WId id)
+{
+    KWindowInfo info(id, NET::WMState);
+    bool isMax = info.hasState(NET::Max);
+
+    m_buttonsWidget->setVisible(isMax);
+}
+
+void AppMenuWidget::minimizeWindow()
+{
+    KWindowSystem::minimizeWindow(KWindowSystem::activeWindow());
+}
+
+void AppMenuWidget::clsoeWindow()
+{
+    NETRootInfo(QX11Info::connection(), NET::CloseWindow).closeWindowRequest(KWindowSystem::activeWindow());
+}
+
+void AppMenuWidget::maxmizeWindow()
+{
+    KWindowSystem::setState(KWindowSystem::activeWindow(), NET::Max);
+}
+
+void AppMenuWidget::restoreWindow()
+{
+    KWindowSystem::clearState(KWindowSystem::activeWindow(), NET::Max);
 }
--- topbar/appmenuwidget.h
+++ topbar/appmenuwidget.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QMenuBar>
+#include <QToolButton>
 #include "appmenu/appmenumodel.h"
 #include "appmenu/menuimporter.h"
 
@@ -14,11 +15,24 @@ public:
     explicit AppMenuWidget(QWidget *parent = nullptr);
 
     void updateMenu();
+    void toggleMaximizeWindow();
+
+private:
+    void onActiveWindowChanged(WId id);
+    void minimizeWindow();
+    void clsoeWindow();
+    void maxmizeWindow();
+    void restoreWindow();
 
 private:
     AppMenuModel *m_appMenuModel;
     MenuImporter *m_menuImporter;
     QMenuBar *m_menuBar;
+
+    QWidget *m_buttonsWidget;
+    QToolButton *m_minButton;
+    QToolButton *m_restoreButton;
+    QToolButton *m_closeButton;
 };
 
 #endif // APPMENUWIDGET_H
