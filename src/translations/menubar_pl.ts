339d2290dc571824e993a894b99b7244fb637516
Weblate <noreply@weblate.org>
2021-03-04 12:26:47 +0100

    Added translation using Weblate (Polish)

---
+++ src/translations/menubar_pl.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl">
+<context>
+    <name>AppMenuWidget</name>
+    <message>
+        <location filename="../appmenuwidget.cpp" line="310"/>
+        <source>About This Computer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BatteryWidget</name>
+    <message>
+        <location filename="../../plugin-battery/batterywidget.cpp" line="103"/>
+        <source>Empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugin-battery/batterywidget.cpp" line="105"/>
+        <source>Discharging</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugin-battery/batterywidget.cpp" line="107"/>
+        <source>Fully charged</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../plugin-battery/batterywidget.cpp" line="110"/>
+        <source>Charging</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ControlWidget</name>
+    <message>
+        <location filename="../../plugin-volume/controlwidget.cpp" line="24"/>
+        <source>Current volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../Shutdown/mainwindow.ui" line="23"/>
+        <location filename="../../Shutdown/mainwindow.ui" line="72"/>
+        <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="104"/>
+        <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="112"/>
+        <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="103"/>
+        <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="111"/>
+        <source>Shut Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Shutdown/mainwindow.ui" line="42"/>
+        <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="106"/>
+        <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="105"/>
+        <source>Restart</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Shutdown/mainwindow.ui" line="49"/>
+        <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="107"/>
+        <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="106"/>
+        <source>Log Out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Shutdown/mainwindow.ui" line="56"/>
+        <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="108"/>
+        <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="107"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Shutdown/mainwindow.ui" line="59"/>
+        <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="110"/>
+        <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="109"/>
+        <source>Esc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Shutdown/mainwindow.ui" line="91"/>
+        <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="113"/>
+        <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="112"/>
+        <source>Are you sure you want to shut down your computer now?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Shutdown/mainwindow.ui" line="110"/>
+        <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="105"/>
+        <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="104"/>
+        <source>Log out</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
