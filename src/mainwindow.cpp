d0d5d7472c08bb95245eb33668d88b0e656fefe5
probonopd <probonopd@users.noreply.github.com>
2021-04-25 10:31:24 +0200

    Ensure that we have the focus when menu is launched so that one can enter text in the search box
    Commenting this out broke it indeed

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -100,8 +100,8 @@ MainWindow::MainWindow(QWidget *parent)
     animation->setEndValue(QPoint(qApp->primaryScreen()->geometry().x(),qApp->primaryScreen()->geometry().y()));
     animation->setEasingCurve(QEasingCurve::OutCubic);
     animation->start(QPropertyAnimation::DeleteWhenStopped);
-    // this->activateWindow(); // probono: Ensure that we have the focus when menu is launched so that one can enter text in the search box
-    // m_mainPanel->raise(); // probono: Trying to give typing focus to the search box that is in there. Needed? Does not seem tp hurt
+    this->activateWindow(); // probono: Ensure that we have the focus when menu is launched so that one can enter text in the search box
+    m_mainPanel->raise(); // probono: Trying to give typing focus to the search box that is in there. Needed? Does not seem tp hurt
 
 }
 
