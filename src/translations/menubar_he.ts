fb4f05a09f5ac897cb75be0ad56550eac1fb0a51
Yaron Shahrabani <sh.yaron@gmail.com>
2021-08-04 12:54:19 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (18 of 18 strings)
    
    Translation: helloSystem/Menu
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/menu/he/

--- src/translations/menubar_he.ts
+++ src/translations/menubar_he.ts
@@ -6,32 +6,32 @@
     <message>
         <location filename="../appmenuwidget.cpp" line="310"/>
         <source>About This Computer</source>
-        <translation type="unfinished"></translation>
+        <translation>המחשב הזה</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="314"/>
         <source>Hide all</source>
-        <translation type="unfinished"></translation>
+        <translation>להסתיר הכול</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="316"/>
         <source>Unhide all</source>
-        <translation type="unfinished"></translation>
+        <translation>להציג הכול</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="330"/>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>הפעלה מחדש</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="332"/>
         <source>Log Out</source>
-        <translation type="unfinished"></translation>
+        <translation>יציאה</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="334"/>
         <source>Shut Down</source>
-        <translation type="unfinished"></translation>
+        <translation>כיבוי</translation>
     </message>
 </context>
 <context>
@@ -39,22 +39,22 @@
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="103"/>
         <source>Empty</source>
-        <translation type="unfinished"></translation>
+        <translation>ריקה</translation>
     </message>
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="105"/>
         <source>Discharging</source>
-        <translation type="unfinished"></translation>
+        <translation>נפרקת</translation>
     </message>
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="107"/>
         <source>Fully charged</source>
-        <translation type="unfinished"></translation>
+        <translation>טעונה במלואה</translation>
     </message>
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="110"/>
         <source>Charging</source>
-        <translation type="unfinished"></translation>
+        <translation>נטענת</translation>
     </message>
 </context>
 <context>
@@ -62,7 +62,7 @@
     <message>
         <location filename="../../plugin-volume/controlwidget.cpp" line="24"/>
         <source>Current volume</source>
-        <translation type="unfinished"></translation>
+        <translation>עצמת שמע נוכחית</translation>
     </message>
 </context>
 <context>
@@ -75,49 +75,49 @@
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="103"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="111"/>
         <source>Shut Down</source>
-        <translation type="unfinished"></translation>
+        <translation>כיבוי</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="42"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="106"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="105"/>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>הפעלה מחדש</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="49"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="107"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="106"/>
         <source>Log Out</source>
-        <translation type="unfinished"></translation>
+        <translation>יציאה</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="56"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="108"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="107"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>ביטול</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="59"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="110"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="109"/>
         <source>Esc</source>
-        <translation type="unfinished"></translation>
+        <translation>Esc</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="91"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="113"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="112"/>
         <source>Are you sure you want to shut down your computer now?</source>
-        <translation type="unfinished"></translation>
+        <translation>לכבות את המחשב שלך כעת?</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="110"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="105"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="104"/>
         <source>Log out</source>
-        <translation type="unfinished"></translation>
+        <translation>יציאה</translation>
     </message>
 </context>
 </TS>
