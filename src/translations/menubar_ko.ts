262912e16f6fa4b864cca3ca7dae665acf07d37b
louies0623 <louies0623@gmail.com>
2021-10-10 09:09:57 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (18 of 18 strings)
    
    Translation: helloSystem/Menu
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/menu/ko/

--- src/translations/menubar_ko.ts
+++ src/translations/menubar_ko.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../appmenuwidget.cpp" line="310"/>
         <source>About This Computer</source>
-        <translation type="unfinished"></translation>
+        <translation>이 컴퓨터</translation>
     </message>
 </context>
 <context>
@@ -14,22 +14,22 @@
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="103"/>
         <source>Empty</source>
-        <translation type="unfinished"></translation>
+        <translation>비우다</translation>
     </message>
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="105"/>
         <source>Discharging</source>
-        <translation type="unfinished"></translation>
+        <translation>출력</translation>
     </message>
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="107"/>
         <source>Fully charged</source>
-        <translation type="unfinished"></translation>
+        <translation>완전 충전</translation>
     </message>
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="110"/>
         <source>Charging</source>
-        <translation type="unfinished"></translation>
+        <translation>충전</translation>
     </message>
 </context>
 <context>
@@ -37,7 +37,7 @@
     <message>
         <location filename="../../plugin-volume/controlwidget.cpp" line="24"/>
         <source>Current volume</source>
-        <translation type="unfinished"></translation>
+        <translation>현재 볼륨입니다</translation>
     </message>
 </context>
 <context>
@@ -50,49 +50,77 @@
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="103"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="111"/>
         <source>Shut Down</source>
-        <translation type="unfinished"></translation>
+        <translation>끄세요</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="42"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="106"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="105"/>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>다시 시작할</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="49"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="107"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="106"/>
         <source>Log Out</source>
-        <translation type="unfinished"></translation>
+        <translation>로그아웃</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="56"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="108"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="107"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>취소</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="59"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="110"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="109"/>
         <source>Esc</source>
-        <translation type="unfinished"></translation>
+        <translation>떠나다</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="91"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="113"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="112"/>
         <source>Are you sure you want to shut down your computer now?</source>
-        <translation type="unfinished"></translation>
+        <translation>지금 종료하시겠습니까?</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="110"/>
         <location filename="../../build-Shutdown-Desktop-Debug/Shutdown_autogen/include/ui_mainwindow.h" line="105"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="104"/>
         <source>Log out</source>
-        <translation type="unfinished"></translation>
+        <translation>로그아웃</translation>
+    </message>
+</context>
+<context>
+    <name></name>
+    <message>
+        <location filename="../appmenuwidget.cpp" line="316"/>
+        <source>Unhide all</source>
+        <translation>모든 숨기기</translation>
+    </message>
+    <message>
+        <location filename="../appmenuwidget.cpp" line="314"/>
+        <source>Hide all</source>
+        <translation>모두 숨기기</translation>
+    </message>
+    <message>
+        <location filename="../appmenuwidget.cpp" line="330"/>
+        <source>Restart</source>
+        <translation>다시 시작할</translation>
+    </message>
+    <message>
+        <location filename="../appmenuwidget.cpp" line="332"/>
+        <source>Log Out</source>
+        <translation>로그 아웃</translation>
+    </message>
+    <message>
+        <location filename="../appmenuwidget.cpp" line="334"/>
+        <source>Shut Down</source>
+        <translation>끄세요</translation>
     </message>
 </context>
 </TS>
