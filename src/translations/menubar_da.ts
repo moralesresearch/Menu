aa2518c4b597b3fa77952ff55a4f638a5df13a60
Lars Henrik Ørn <larshenrikoern@outlook.dk>
2021-10-23 09:59:49 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (20 of 20 strings)
    
    Translation: helloSystem/Menu
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/menu/da/

--- src/translations/menubar_da.ts
+++ src/translations/menubar_da.ts
@@ -7,32 +7,32 @@
         <location filename="../appmenuwidget.cpp" line="312"/>
         <location filename="../appmenuwidget.cpp" line="609"/>
         <source>About This Computer</source>
-        <translation type="unfinished"></translation>
+        <translation>Denne computer</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="316"/>
         <source>Hide all</source>
-        <translation type="unfinished"></translation>
+        <translation>Skjul alt</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="318"/>
         <source>Unhide all</source>
-        <translation type="unfinished"></translation>
+        <translation>Gør alle synlige</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="332"/>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>Genstart</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="334"/>
         <source>Log Out</source>
-        <translation type="unfinished"></translation>
+        <translation>Log ud</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="336"/>
         <source>Shut Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Luk ned</translation>
     </message>
 </context>
 <context>
@@ -40,22 +40,22 @@
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="103"/>
         <source>Empty</source>
-        <translation type="unfinished"></translation>
+        <translation>Tom</translation>
     </message>
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="105"/>
         <source>Discharging</source>
-        <translation type="unfinished"></translation>
+        <translation>Aflader</translation>
     </message>
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="107"/>
         <source>Fully charged</source>
-        <translation type="unfinished"></translation>
+        <translation>Fuldt opladet</translation>
     </message>
     <message>
         <location filename="../../plugin-battery/batterywidget.cpp" line="110"/>
         <source>Charging</source>
-        <translation type="unfinished"></translation>
+        <translation>Oplader</translation>
     </message>
 </context>
 <context>
@@ -63,7 +63,7 @@
     <message>
         <location filename="../../plugin-volume/controlwidget.cpp" line="24"/>
         <source>Current volume</source>
-        <translation type="unfinished"></translation>
+        <translation>Nuværende disk</translation>
     </message>
 </context>
 <context>
@@ -74,44 +74,44 @@
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="100"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="108"/>
         <source>Shut Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Luk ned</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="42"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="102"/>
         <source>Restart</source>
-        <translation type="unfinished"></translation>
+        <translation>Genstart</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="49"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="103"/>
         <location filename="../mainwindow.cpp" line="44"/>
         <source>Log Out</source>
-        <translation type="unfinished"></translation>
+        <translation>Log ud</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="56"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="104"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Fortryd</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="59"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="106"/>
         <source>Esc</source>
-        <translation type="unfinished"></translation>
+        <translation>Esc</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="91"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="109"/>
         <source>Are you sure you want to shut down your computer now?</source>
-        <translation type="unfinished"></translation>
+        <translation>Er du sikker på at du vil slukke computeren nu?</translation>
     </message>
     <message>
         <location filename="../../Shutdown/mainwindow.ui" line="110"/>
         <location filename="../../Shutdown/build/Shutdown_autogen/include/ui_mainwindow.h" line="101"/>
         <source>Log out</source>
-        <translation type="unfinished"></translation>
+        <translation>Log ud</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="45"/>
