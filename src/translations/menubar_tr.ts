0389d93f5ff091f7cf9ce21bc0bb287c1e5a51c2
JSEHV <jorren.schauwaert@gmail.com>
2021-11-18 21:22:05 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: helloSystem/Menu
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/menu/tr/

--- src/translations/menubar_tr.ts
+++ src/translations/menubar_tr.ts
@@ -29,7 +29,7 @@
     <message>
         <location filename="../appmenuwidget.cpp" line="282"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Ara</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="312"/>
@@ -144,7 +144,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="45"/>
         <source>About This Computer</source>
-        <translation type="unfinished">Bu bilgisayar</translation>
+        <translation>Bu Bilgisayar Hakkında</translation>
     </message>
 </context>
 <context>
@@ -152,7 +152,7 @@
     <message>
         <location filename="../appmenuwidget.cpp" line="108"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtre Arama</translation>
     </message>
 </context>
 </TS>
