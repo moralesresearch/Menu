0a3f40806610edd3b18e857085a5be8ddd993d41
JSEHV <jorren.schauwaert@gmail.com>
2021-11-18 21:21:12 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: helloSystem/Menu
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/menu/ru/

--- src/translations/menubar_ru.ts
+++ src/translations/menubar_ru.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../appmenuwidget.cpp" line="282"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Поиск</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="312"/>
@@ -121,7 +121,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="45"/>
         <source>About This Computer</source>
-        <translation type="unfinished">Этот компьютер</translation>
+        <translation>Этот компьютер</translation>
     </message>
 </context>
 <context>
@@ -129,7 +129,7 @@
     <message>
         <location filename="../appmenuwidget.cpp" line="108"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Фильтр поиска</translation>
     </message>
 </context>
 </TS>
