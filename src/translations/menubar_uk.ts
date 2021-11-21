7256aabe2e6ff5e6853e78444aff0214c3648120
Taras Panchenko <top39@ukr.net>
2021-11-21 14:00:57 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: helloSystem/Menu
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/menu/uk/

--- src/translations/menubar_uk.ts
+++ src/translations/menubar_uk.ts
@@ -12,7 +12,7 @@
         <location filename="../appmenuwidget.cpp" line="312"/>
         <location filename="../appmenuwidget.cpp" line="609"/>
         <source>About This Computer</source>
-        <translation>Комп&apos;ютер</translation>
+        <translation>Про цей Комп&apos;ютер</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="316"/>
