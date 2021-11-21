351d190ee85f976ffe1a904c9dd3d3280870aff9
Allan Nordhøy <epost@anotheragency.no>
2021-11-21 05:38:06 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 90.4% (19 of 21 strings)
    
    Translation: helloSystem/Menu
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/menu/nb_NO/

--- src/translations/menubar_nb_NO.ts
+++ src/translations/menubar_nb_NO.ts
@@ -29,13 +29,13 @@
     <message>
         <location filename="../appmenuwidget.cpp" line="282"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Søk</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="312"/>
         <location filename="../appmenuwidget.cpp" line="609"/>
         <source>About This Computer</source>
-        <translation>Denne datamaskinen</translation>
+        <translation>Om denne datamaskinen</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="316"/>
@@ -144,7 +144,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="45"/>
         <source>About This Computer</source>
-        <translation type="unfinished">Denne datamaskinen</translation>
+        <translation>Om denne datamaskinen</translation>
     </message>
 </context>
 <context>
@@ -152,7 +152,7 @@
     <message>
         <location filename="../appmenuwidget.cpp" line="108"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Filtrer søk</translation>
     </message>
 </context>
 </TS>
