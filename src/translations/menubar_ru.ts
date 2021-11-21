40a0b45bd65ac470f68f3b155990a5c00bb0c904
Taras Panchenko <top39@ukr.net>
2021-11-21 14:01:41 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: helloSystem/Menu
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/menu/ru/

--- src/translations/menubar_ru.ts
+++ src/translations/menubar_ru.ts
@@ -12,7 +12,7 @@
         <location filename="../appmenuwidget.cpp" line="312"/>
         <location filename="../appmenuwidget.cpp" line="609"/>
         <source>About This Computer</source>
-        <translation>Этот компьютер</translation>
+        <translation>Про этот Компьютер</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="316"/>
