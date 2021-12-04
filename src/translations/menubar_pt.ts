125c8bf9e2b8fe3b8730e99d9dc576591adf0dff
Hugo Carvalho <hugokarvalho@hotmail.com>
2021-12-04 22:24:55 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: helloSystem/Menu
    Translate-URL: https://hosted.weblate.org/projects/hellosystem/menu/pt/

--- src/translations/menubar_pt.ts
+++ src/translations/menubar_pt.ts
@@ -29,13 +29,13 @@
     <message>
         <location filename="../appmenuwidget.cpp" line="282"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Procurar</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="312"/>
         <location filename="../appmenuwidget.cpp" line="609"/>
         <source>About This Computer</source>
-        <translation>Este Computador</translation>
+        <translation>Acerca deste computador</translation>
     </message>
     <message>
         <location filename="../appmenuwidget.cpp" line="316"/>
@@ -144,7 +144,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="45"/>
         <source>About This Computer</source>
-        <translation type="unfinished">Este Computador</translation>
+        <translation>Acerca deste computador</translation>
     </message>
 </context>
 <context>
@@ -152,7 +152,7 @@
     <message>
         <location filename="../appmenuwidget.cpp" line="108"/>
         <source>Search</source>
-        <translation type="unfinished"></translation>
+        <translation>Filtro de pesquisa</translation>
     </message>
 </context>
 </TS>
